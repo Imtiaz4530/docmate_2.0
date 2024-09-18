913dc88ee7e4669f9155430d48478e8474aee119
Imtiaz4530 <smimtiaz58@gmail.com>
2024-09-18 17:27:20 +0600

    mongoose version

--- server/db/mongoConnection.js
+++ server/db/mongoConnection.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI);
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log("Database Connected 😍");
 
